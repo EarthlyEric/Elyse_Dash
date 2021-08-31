a96bce36ad173dc166f59f63327e0bec71f40edb
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2021-08-31 12:54:58 +0800

    ~

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import os
 from flask import Flask,render_template,redirect,url_for
 from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
 
+
 deploy=False
 
 app=Flask(__name__,static_url_path='')
 
 
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.route('/login')
+def login():
+    return app.send_static_file('login.html')
+
+@app.route('/login/oauth2')
+def login():
+    pass
 
 
 
